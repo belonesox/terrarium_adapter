9f0ccb6eaee4a4cb5e8d302f827c139584f8ab3d
stas <stas-fomin@yandex.ru>
2020-12-11 15:51:04 +0300

    Now tested on Astra 1.5 + 1.6, several Ubuntus, ALT SP8

---
+++ terrarium_adapter/terrarium-adapter-run.py
@@ -0,0 +1,24 @@
+# script to be built with nuitka
+import terrarium_adapter  
+import subprocess
+
+# Тестирование адаптера. Запуск «террариумных и внетеррариумных бинарников»
+# Для проверки на виртуалках внешних систем
+# Включая компиляцию с нуиткой.
+
+for cmds_ in [
+        ('ls', '-l', '/'),
+        ('uname', '-a'),
+        ('gsettings', '--version'),
+        ('compton', '--version'),
+        ('gs', '-v'),
+        ('python3.8', '-V'),
+            ]:
+    print('*'*20, ' '.join(cmds_) + ':\n')
+    try:
+        ls = subprocess.check_output(cmds_)
+        print(ls)
+    except Exception as ex_:
+        print('Failed to run!')
+    print('-'*20)
+
